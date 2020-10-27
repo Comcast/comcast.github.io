66bf226dbf004b7fc1d1aa5e665b7b168280ec31
Nathan Lilly <nathan_lilly@comcast.com>
2020-10-27 10:18:57 -0400

    Add .eslintignore for docs

---
+++ docs/brand_icons/Atom.jsx
@@ -0,0 +1,11 @@
+const Atom = () => (
+  <svg viewBox="0 0 100 100">
+    <ellipse cx="50" cy="50" rx="14.69" ry="39.05" />
+    <ellipse cx="50" cy="50" rx="14.69" ry="39.05" />
+    <ellipse cx="50" cy="50" rx="39.05" ry="14.69" transform="rotate(-31.838 50.003 50.003)" />
+    <ellipse cx="50" cy="50" rx="14.69" ry="39.05" transform="rotate(-58.902 50.003 50)" />
+    <circle cx="50" cy="50.95" r="6.25" />
+  </svg>
+);
+
+export default Atom;
