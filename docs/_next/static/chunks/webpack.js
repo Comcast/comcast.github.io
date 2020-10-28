e8663cbce07a66a7e482ce4bfe928cfb852674fb
Nathan Lilly <nathan_lilly@comcast.com>
2020-10-28 18:05:39 -0400

    Deploy docs

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "96d4bbd2f7004f24e3c6";
+/******/ 	var hotCurrentHash = "1190ce5a889bbf580cbb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
