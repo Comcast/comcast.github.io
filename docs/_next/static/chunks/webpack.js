f81c19ff6683dab0ddcc460aa4102d2c1cc1e778
Nathan Lilly <nathan_lilly@comcast.com>
2020-11-09 19:06:24 -0500

    Deploy docs

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c01b4316be1cfeca198f";
+/******/ 	var hotCurrentHash = "3770946db8ad87736cf8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ docs/_next/static/webpack/pages/blog.d0d73578340ac3282b8a.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/blog",{
+
+/***/ "./src/data/blog.json":
+/*!****************************!*\
+  !*** ./src/data/blog.json ***!
+  \****************************/
+/*! exports provided: title, overview, description, blogList, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"title\\\":\\\"Comcast Open Source Blog\\\",\\\"overview\\\":\\\"\\\",\\\"description\\\":\\\"The Comcast Labs blog is a place where Comcast technologists talk about the work they are doing, the products, infrastructure, and services they are building, and the trends and developments that are changing how we experience entertainment.\\\",\\\"blogList\\\":[{\\\"title\\\":\\\"Google's Kelsey Hightower offers tips on how to centralize and evolve IT practices by Matt Asay\\\",\\\"date\\\":\\\"October 16, 2020\\\",\\\"url\\\":\\\"https://www.techrepublic.com/article/googles-kelsey-hightower-offers-tips-on-how-to-centralize-and-evolve-it-practices/\\\",\\\"image\\\":\\\"/images/blog/femaleengineer-istock-1146417905-metamorworks.jpg\\\"},{\\\"title\\\":\\\"Podcast: The Business of Cloud Native with Emily Omier and Nithya Ruff\\\",\\\"date\\\":\\\"October 14, 2020\\\",\\\"url\\\":\\\"https://open.spotify.com/show/5ZGTCyhjpnyZk5RDVH4WjS\\\",\\\"image\\\":\\\"\\\"},{\\\"title\\\":\\\"GPL Initiative Grows Over 40%; More Than 60 Companies Have Joined the Campaign for Greater Predictability in Open Source Licensing\\\",\\\"date\\\":\\\"September 11, 2020\\\",\\\"url\\\":\\\"https://www.redhat.com/en/about/press-releases/gpl-initiative-grows-over-40-more-60-companies-have-joined-campaign-greater-predictability-open-source-licensing?sc_cid=701f2000000tyBjAAI\\\",\\\"image\\\":\\\"\\\"},{\\\"title\\\":\\\"Meet these 12 tech companies and see who’s hiring at the Sept. 23 Developers Conference\\\",\\\"date\\\":\\\"September 10, 2020\\\",\\\"url\\\":\\\"https://technical.ly/philly/2020/09/10/meet-11-tech-companies-hiring-september-23-developers-conference-philly-tech-week-2020/\\\",\\\"image\\\":\\\"\\\"}]}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3NyYy9kYXRhL2Jsb2cuanNvbi5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/data/blog.json\n");
+
+/***/ })
+
+})
