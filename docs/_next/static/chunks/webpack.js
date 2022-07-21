d52ff660799dcc1dc9029b0bb6863f9bea829bb8
Klay-Michael Dehaney <kdehaney53@gmail.com>
2022-07-21 12:07:16 -0400

    Deploy docs

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a1e6860e6cb2511d950d";
+/******/ 	var hotCurrentHash = "e2756a80215d317582b6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
