d080b52a75ab37c105f2983fa314f2fd6f6d82bf
AadithyaHariharan <143205525+Webaadhi5E@users.noreply.github.com>
2025-04-15 12:52:31 +0000

    next.config file changes

--- next.config.js
+++ next.config.js
@@ -1,35 +1,58 @@
 const webpack = require('webpack');
-
-const isProd = process.env.NODE_ENV === 'production';
-const assetPrefix = isProd ? '/ospo1' : ''; // Ensure assets load from correct path
+// const path = require('path');
+const isProd = (process.env.NODE_ENV || 'production') === 'production';
+const assetPrefix = isProd ? '/comcast.github.io' : '';
 
 module.exports = {
-  output: 'export',
-  assetPrefix,
-  trailingSlash: true, // Ensures all static routes end with a `/`
-  images: {
-    unoptimized: true, // Fixes `next/image` for static exports
-  },
+  // future: {
+  //   webpack5: true,
+  // },
+  // node: {
+  //   __dirname: true,
+  //   __filename: true,
+  // },
+  // resolve: {
+  //   alias: {
+  //     src: path.resolve(__dirname, 'src'),
+  //     test: path.resolve(__dirname, 'test'),
+  //   },
+  // },
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: '[local]___[hash:base64:5]',
     url: false,
   },
+  // exportPathMap() {
+  //   return {
+  //     '/src/pages/': { page: '/' },
+  //     '/src/pages/blog': { page: '/blog' },
+  //     '/src/pages/community': { page: '/community' },
+  //     '/src/pages/events': { page: '/events' },
+  //     '/src/pages/people': { page: '/people' },
+  //     '/src/pages/projects': { page: '/projects' },
+  //   };
+  // },
+  assetPrefix,
   webpack: (config) => {
     config.plugins.push(
       new webpack.DefinePlugin({
         'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
       }),
     );
-
     config.resolve.modules.push(__dirname);
-
     config.module.rules.push({
       test: /\.svg$/,
       use: [
-        { loader: 'babel-loader' },
-        { loader: 'react-svg-loader', options: { jsx: true } },
+        {
+          loader: 'babel-loader',
+        },
+        {
+          loader: 'react-svg-loader',
+          options: {
+            jsx: true, // true outputs JSX tags
+          },
+        },
       ],
     });
 
